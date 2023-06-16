12f425ee5c7d8ef71ab50768f957d9a3a984837b
Moe Adel <imoedev@gmail.com>
2023-06-16 16:44:56 +0300

    removing unnecessary files

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -11,7 +11,6 @@ const PageTemplate = ({ children }) => {
         className="w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark 
                    xl:p-24 lg:p-16 md:p-12 sm:p-8"
       >
-        {/* <TransitionEffect /> */}
         {children}
       </div>
       <Footer />
